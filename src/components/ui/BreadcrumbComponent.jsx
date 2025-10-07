f4cf9a45075d09e47c8bde45045308ede4d39d3b
development-nex <development@nexenstial.com>
2025-10-07 10:50:21 +0530

    feat: Add breadcrumb component.

---
+++ src/components/ui/BreadcrumbComponent.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+
+import { Link } from "@tanstack/react-router";
+import { Breadcrumb } from "antd";
+import { cn } from "../utils/classname.util";
+/**
+ * @param {import("antd").BreadcrumbProps} props
+ * @param {import("antd").BreadcrumbItemProps} props
+ */
+const BreadcrumbComponent = ({ items, className, ...rest }) => {
+  return (
+    <div className={cn(className)}>
+      <Breadcrumb
+        className="text-sm"
+        items={items?.map((item) => {
+          if (item.href) {
+            return {
+              title: <Link to={item.href}>{item.title}</Link>,
+            };
+          }
+          return {
+            title: item.title,
+          };
+        })}
+        {...rest}
+      />
+    </div>
+  );
+};
+
+export default BreadcrumbComponent;
