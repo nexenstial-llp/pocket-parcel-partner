70c5aadeff452bd8a0ba5497613bdfdc67247948
development-nex <development@nexenstial.com>
2025-10-11 09:27:30 +0530

    feat: Add loader component.

---
+++ src/components/layout/Loader.jsx
@@ -0,0 +1,19 @@
+import Lottie from "react-lottie";
+import animationData from "../../lotties/TRUCK_LOADING.json";
+const Loader = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  return (
+    <div>
+      <Lottie options={defaultOptions} height={400} width={400} />
+    </div>
+  );
+};
+
+export default Loader;
