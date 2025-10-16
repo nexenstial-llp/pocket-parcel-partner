380d74c6372f96e3ec95d15b07798eb06f5233ab
development-nex <development@nexenstial.com>
2025-10-16 09:21:45 +0530

    feat: Add breadcrumb component in the navbar.

--- src/components/layout/PageLayout.jsx
+++ src/components/layout/PageLayout.jsx
@@ -1,9 +1,21 @@
 /* eslint-disable react/prop-types */
 import { cn } from "../../utils/classname.util";
+import BreadcrumbComponent from "../ui/BreadcrumbComponent";
 
-const PageLayout = ({ className, children }) => {
+/**
+ * A layout component that wraps a BreadcrumbComponent and children
+ * with a margin of 5 and a flex column layout with a gap of 4.
+ * @param {string} className - Additional CSS classes to apply to the component
+ * @param {React.ReactNode} children - The children elements to render
+ * @param {import("antd").BreadcrumbProps["items"]} items - The items to render in the breadcrumb
+ * @returns {React.ReactElement} - The rendered component
+ */
+const PageLayout = ({ className, children, items = [] }) => {
   return (
-    <div className={cn("m-5 flex flex-col gap-4", className)}>{children}</div>
+    <div className={cn("m-5 flex flex-col gap-4", className)}>
+      {items.length > 0 && <BreadcrumbComponent items={items} />}
+      {children}
+    </div>
   );
 };
 
