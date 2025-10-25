994fa651101d38b65d08fb10e46b1d0a24f89989
development-nex <development@nexenstial.com>
2025-10-25 15:39:51 +0530

    refactor: prettify the code.

--- src/routes/_authenticated/dashboard/domestic/route.jsx
+++ src/routes/_authenticated/dashboard/domestic/route.jsx
@@ -3,33 +3,34 @@ import {
   Outlet,
   useLocation,
   useNavigate,
-} from '@tanstack/react-router'
-import { Menu } from 'antd'
+} from "@tanstack/react-router";
+import { Menu } from "antd";
 
-export const Route = createFileRoute('/_authenticated/dashboard/domestic')({
+export const Route = createFileRoute("/_authenticated/dashboard/domestic")({
   component: RouteComponent,
-})
+});
 
 const items = [
-  { key: '/dashboard/domestic/overview', label: 'Overview' },
-  { key: '/dashboard/domestic/orders', label: 'Orders' },
-  { key: '/dashboard/domestic/shipments', label: 'Shipments' },
-  { key: '/dashboard/domestic/ndr', label: 'NDR' },
-  { key: '/dashboard/domestic/rto', label: 'RTO' },
-  { key: '/dashboard/domestic/courier', label: 'Courier' },
-  { key: '/dashboard/domestic/delays', label: 'Delays' },
-]
+  { key: "/dashboard/domestic/overview", label: "Overview" },
+  { key: "/dashboard/domestic/orders", label: "Orders" },
+  { key: "/dashboard/domestic/shipments", label: "Shipments" },
+  { key: "/dashboard/domestic/ndr", label: "NDR" },
+  { key: "/dashboard/domestic/rto", label: "RTO" },
+  { key: "/dashboard/domestic/courier", label: "Courier" },
+  { key: "/dashboard/domestic/delays", label: "Delays" },
+];
 
 function RouteComponent() {
-  const navigate = useNavigate()
-  const location = useLocation()
-  const activeKey = location.pathname
+  const navigate = useNavigate();
+  const location = useLocation();
+  const activeKey = location.pathname;
   const onClick = (e) => {
-    navigate({ to: `${e.key}` })
-  }
+    navigate({ to: `${e.key}` });
+  };
   return (
     <>
       <Menu
+        className="button-menu"
         onClick={onClick}
         selectedKeys={[activeKey]}
         mode="horizontal"
@@ -39,5 +40,5 @@ function RouteComponent() {
 
       <Outlet />
     </>
-  )
+  );
 }
