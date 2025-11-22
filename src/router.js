c7fb73f0f30f8cdaced0cd56a621edac866efb81
development-nex <development@nexenstial.com>
2025-11-22 13:58:41 +0530

    feat: Add default preload.

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 export const router = createRouter({
   routeTree,
+  // defaultPreload: "intent",
   context: {
     auth: undefined,
   },
